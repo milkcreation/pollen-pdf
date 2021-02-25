9b68f419bd049303a0a3581f7b914fe42f024492
5upp0r7 <5upp0r7@23fad6b8-6d8e-48a5-9e8d-e4f1c0242fc1>
2021-02-25 10:11:03 +0000

    pdf >> 1.0.x-dev

--- pdf/branches/1.0/src/PdfInterface.php
+++ pdf/branches/1.0/src/PdfInterface.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Pollen\Pdf;
 
 /**
- * @mixin \Pollen\Support\Concerns\ConfigBagTrait;
+ * @mixin \Pollen\Support\Concerns\ConfigBagAwareTrait;
  * @mixin \Pollen\Support\Concerns\ContainerAwareTrait;
  */
 interface PdfInterface
